9cadd2af5eff8e1cc92d66571f555737b9c21f07
agronkabashi <agron@cerberussolutions.se>
2017-01-31 15:51:21 +0100

    Build files were synchronized

--- build/rollup-plugin-conditional.js
+++ build/rollup-plugin-conditional.js
@@ -1,5 +1,54 @@
 'use strict';
 
-var index = (options = {}) => options.condition && options.plugin || (() => {});
+const pluginApi = [
+  'load',
+  'resolveId',
+  'transform',
+  'transformBundle'
+];
+
+const pluginApiCallbacks = [
+  'ongenerate',
+  'onwrite'
+];
+
+const pluginApiStringInjections = [
+  'intro',
+  'outro',
+  'banner',
+  'footer'
+];
+
+function getFirstExecutablePlugin(apiName, plugins, ...args) {
+  // Will return the result of the very first plugin that supports the api call and doesn't return a falsy value
+  return plugins.reduce((result, plugin) => result || plugin[apiName] && plugin[apiName](...args), undefined);
+}
+
+var index = (options = {}) => {
+  if (!options.condition || !Array.isArray(options.plugins) || options.plugins.length === 0) {
+    return {};
+  }
+
+  const { plugins } = options;
+
+  const constructedApi = {};
+
+  pluginApi.reduce((api, apiName) => {
+    api[apiName] = (...args) => getFirstExecutablePlugin(apiName, plugins, ...args);
+    return api;
+  }, constructedApi);
+
+  pluginApiCallbacks.reduce((api, apiName) => {
+    api[apiName] = (...args) => plugins.forEach(plugin => plugin[apiName] && plugin[apiName](...args));
+    return api;
+  }, constructedApi);
+
+  pluginApiStringInjections.reduce((api, apiName) => {
+    api[apiName] = (...args) => plugins.reduce((result, plugin) => result + (plugin[apiName] && plugin[apiName](...args) || ''), '');
+    return api;
+  }, constructedApi);
+
+  return constructedApi;
+};
 
 module.exports = index;
